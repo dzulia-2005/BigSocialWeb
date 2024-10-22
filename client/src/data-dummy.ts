44becae402a06d9642613c1cfae7ec3f464c3448
dzulia-2005 <159149786+dzulia-2005@users.noreply.github.com>
2024-10-22 19:41:06 +0400

    add api in proj

--- client/src/data-dummy.ts
+++ client/src/data-dummy.ts
@@ -3,6 +3,16 @@ export const User = [
         id:1,
         profilepicture:"https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_3.jpg",
         username:"rudolf arakelyan"
+    },
+    {
+        id:1,
+        profilepicture:"https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_3.jpg",
+        username:"rudolf arakelyan"
+    },
+    {
+        id:1,
+        profilepicture:"https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_3.jpg",
+        username:"rudolf arakelyan"
     }
 ]
 
@@ -12,7 +22,7 @@ export const Posts = [
         desc:"hello world",
         img:"https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_1080,q_100,w_1080/v1/gcs/platform-data-dsc/events/web_ilY3NoA.jpeg",
         date:"5 minutes ago",
-        like:0,
+        like:1,
         comment:9,
         userid:1,
     }
