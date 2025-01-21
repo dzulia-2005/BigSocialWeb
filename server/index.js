59fa1275f70907583ee30b63d4c2af8c6b7e90b1
dzulia-2005 <159149786+dzulia-2005@users.noreply.github.com>
2025-01-22 01:18:43 +0400

    update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const connectDB = require("./database/db");
 const dotenv = require("dotenv");
 const cookieparser = require("cookie-parser");
-const {errorhandler} = require("./middlewares/errors");
+const { errorhandler } = require("./middlewares/errors");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/post");
@@ -13,34 +13,37 @@ const path = require("path");
 const verifyToken = require("./middlewares/verifyToken");
 const cors = require("cors");
 
-
-const app = express();
 dotenv.config();
 
-app.use(
-    cors({
-      origin: "*", 
-    })
-  );
+const app = express();
 
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(cookieparser());
-app.use("/uploads",express.static(path.join(__dirname,"uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use("/api/auth",authRoute);
-app.use("/api/user",verifyToken,userRoute);
-app.use("/api/post",verifyToken,postRoute);
-app.use("/api/comments",verifyToken,commentRoute);
-app.use("/api/conversation",verifyToken,conversationRoute);
-app.use("/api/message",verifyToken,messageRoute);
+// Routes
+app.use("/api/auth", authRoute);
+app.use("/api/user", verifyToken, userRoute);
+app.use("/api/post", verifyToken, postRoute);
+app.use("/api/comments", verifyToken, commentRoute);
+app.use("/api/conversation", verifyToken, conversationRoute);
+app.use("/api/message", verifyToken, messageRoute);
 
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from Express on Vercel!' });
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from Express on Vercel!" });
 });
+
 
 app.use(errorhandler);
 
-app.listen(process.env.PORT,()=>{
-    connectDB()
-    console.log("Server running on port 3000");
-})
+
+connectDB();
+
+
+module.exports = app;
