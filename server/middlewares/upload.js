30a6af22bf9c79a66cf24524bcdf2c0a4f34b1fb
dzulia-2005 <159149786+dzulia-2005@users.noreply.github.com>
2025-01-24 18:14:38 +0400

    added upload folder

--- server/middlewares/upload.js
+++ server/middlewares/upload.js
@@ -1,16 +1,15 @@
 const multer = require("multer");
-const path = require("path");
 
 const storage = multer.diskStorage({
-    destination:function(req,file,cb){
-        cb(null,"uploads/")
+    destination: (req, file, cb) => {
+        cb(null, "uploads/"); // სად ინახება ფაილები
     },
-    filename:function(req,file,cb){
-        const ext = path.extname(file.originalname);
-        cb(null,`${file.fieldname}-${Date.now()}${ext}`)
-    }
-})
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        cb(null, uniqueSuffix + "-" + file.originalname); // უნიკალური სახელი
+    },
+});
 
-const upload = multer({storage:storage});
+const upload = multer({ storage: storage });
 
 module.exports = upload;
